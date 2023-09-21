90503ba2783fa3750d481d26d9e7efd9d1fae4d2
joshua chivile <joshuachivile@gmail.com>
2023-09-21 17:52:22 +0300

    Completed Anonymous functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(arg='roller-skate'){
+    return `This Saturday, I want to ${arg}!`
+}
+
+function mondayWork(arg='go to the office'){
+    return `This Monday, I will ${arg}.`
+}
+
+function wrapAdjective(arg1){
+    const inner = function (arg2='special'){
+        return `You are ${arg1}${arg2}${arg1}!`
+    }
+    return inner;
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
